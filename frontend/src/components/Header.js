0ac6883937ee9dfe91bc45a6d54ec111eb6b03f9
Eva Patel <133888581+evapatel1654@users.noreply.github.com>
2025-03-10 07:38:18 +0530

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1>TeaRAG</h1>
+        <p>Smart Tea Recommendations with RAG Technology</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
